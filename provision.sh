e65cbb1bc679ef8f317f2e8352b1f0f34c04d95c
Daniel Silvestre <daniel.silvestre@locaweb.com.br>
2014-10-28 22:48:12 -0200

    Improving prov script - unfinished but WIP

--- provision.sh
+++ provision.sh
@@ -1,9 +1,57 @@
 #!/bin/bash
 
 RETVAL=0
-PROV_HOME_DIR=/home/dani
 
-first() {
+is_gdrive_working() {
+  if [ ! -d "~/gdrive" ] || [ ! -d "~/gdrive/ssh" ] || [ ! -d "~/gdrive/ssh/vpn" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+check_gdrive() {
+  if ! is_gdrive_working; then
+    echo "O Gdrive não está configurado ou inicializado. Abortando o processo"
+    exit 1
+  fi
+}
+
+update_pkgs() {
+  force_update=$1
+
+  if [ "$force_update" = true ] || [ "$PROV_APT_UPDATED" != true ]; then
+    echo "Atualizando lista de pacotes"
+    sudo apt-get update
+    export PROV_APT_UPDATED=true
+  fi
+}
+
+install_pkgs() {
+  pkgs=$1
+
+  if [ -z "$pkgs" ]; then
+    echo "É necessário passar os pacotes separados por espaço como argumento"
+    exit 1
+  fi
+
+  update_pkgs
+
+  echo "Instalando pacotes $pkgs"
+  sudo apt-get --yes --force-yes install $pkgs
+}
+
+first_step_done() {
+  if [ "$PROV_FIRST_ROOT_PASS" != true ] && [ "$PROV_FIRST_UPDATE" = true ]; then
+    echo 'first_step_done...true'
+    return 0
+  else
+    echo 'first_step_done...false'
+    return 1
+  fi
+}
+
+first_step() {
   if [ "$PROV_FIRST_ROOT_PASS" = true ]; then
     echo "A senha do root já foi configurada"
   else
@@ -15,7 +63,7 @@ first() {
     done
 
     export PROV_FIRST_ROOT_PASS=true
-    echo 'export PROV_FIRST_ROOT_PASS=true' >> $PROV_HOME_DIR/.bash_profile
+    echo 'export PROV_FIRST_ROOT_PASS=true' >> ~/.bash_profile
   fi
 
   if [ "$PROV_FIRST_UPDATE" = true ]; then
@@ -26,17 +74,26 @@ first() {
     sudo apt-get --yes --force-yes upgrade
 
     export PROV_FIRST_UPDATE=true
-    echo 'export PROV_FIRST_UPDATE=true' >> $PROV_HOME_DIR/.bash_profile
+    echo 'export PROV_FIRST_UPDATE=true' >> ~/.bash_profile
 
     echo "Reiniciando para aplicar todas atualizações"
     sudo reboot
   fi
 }
 
-git() {
+git_step_done() {
+  if false; then
+    echo 'git_step_done...true'
+    return 0
+  else
+    echo 'git_step_done...false'
+    return 1
+  fi
+}
+
+git_step() {
   echo "Atualizando e instalando o git"
-  sudo apt-get update
-  sudo apt-get --yes --force-yes install git meld gitk
+  install_pkgs 'git meld gitk'
 
   echo "Configurando o git"
   sudo rm -f /usr/local/bin/git-diff.sh
@@ -60,40 +117,61 @@ git() {
   ln -s ~/.vim/vimrc ~/.vimrc
 }
 
-grive() {
-  if [ -e "/etc/apt/sources.list.d/thefanclub-grive-tools-trusty.list" ]; then
-    echo "Repositório para instalação do Grive já existe. Pulando sua inclusão"
+# grive() {
+#   if [ -e "/etc/apt/sources.list.d/thefanclub-grive-tools-trusty.list" ]; then
+#     echo "Repositório para instalação do Grive já existe. Pulando sua inclusão"
+#   else
+#     echo "Adicionando repositório para instalação do Grive (Google Drive Client)"
+#     sudo apt-add-repository ppa:thefanclub/grive-tools
+#   fi
+# 
+#   sudo apt-get update
+#   sudo apt-get --yes --force-yes install grive-tools
+# 
+#   echo "Agora configure o Grive (Grive Setup no launcher) e faça o sync completo da nuvem..."
+# }
+
+gdrive_step_done() {
+  if false; then
+    echo 'gdrive_step_done...true'
+    return 0
   else
-    echo "Adicionando repositório para instalação do Grive (Google Drive Client)"
-    sudo apt-add-repository ppa:thefanclub/grive-tools
+    echo 'gdrive_step_done...false'
+    return 1
   fi
-
-  sudo apt-get update
-  sudo apt-get --yes --force-yes install grive-tools
-
-  echo "Agora configure o Grive (Grive Setup no launcher) e faça o sync completo da nuvem..."
 }
 
-gdrive() {
-  if [ -e "/etc/apt/sources.list.d/alessandro-strada-ppa-precise.list" ]; then
+gdrive_step() {
+  if [ -e "/etc/apt/sources.list.d/alessandro-strada-ppa-trusty.list" ] || [ -d "~/gdrive" ]; then
     echo "Repositório para instalação do Gdrive já existe. Pulando sua inclusão"
   else
-    echo "Adicionando repositório para instalação do Gdrive (Google Drive Client)"
+    echo "Adicionando repositório para instalação do Gdrive (google-drive-ocamlfuse)"
     sudo apt-add-repository ppa:alessandro-strada/ppa
   fi
 
-  sudo apt-get update
-  sudo apt-get --yes --force-yes install grive-tools
+  install_pkgs 'google-drive-ocamlfuse'
 
-  mkdir -p ~/googledrive
-  sudo usermod -a -G fuse daniel
-  exec su -l $USER
+  mkdir -p ~/gdrive
+  sudo usermod -a -G fuse $USER
   echo "Agora configure o Gdrive (google-drive-ocamlfuse) e monte na pasta (google-drive-ocamlfuse ~/gdrive). Para desmontar (fusermount -u ~/gdrive)"
   echo "Mais detalhes tais como automount em http://xmodulo.com/2013/10/mount-google-drive-linux.html"
+  exec su -l $USER
 }
 
-ssh() {
-  echo "Copiando chaves ssh e configurando pasta no home (depende do sync do Drive)"
+ssh_step_done() {
+  if false; then
+    echo 'ssh_step_done...true'
+    return 0
+  else
+    echo 'ssh_step_done...false'
+    return 1
+  fi
+}
+
+ssh_step() {
+  check_gdrive
+
+  echo "Copiando chaves ssh e configurando pasta no home"
   mkdir -p ~/.ssh
   cp -R ~/gdrive/ssh/* ~/.ssh
   chmod 600 ~/.ssh/vpn/*
@@ -101,19 +179,31 @@ ssh() {
   chmod 700 ~/.ssh/vpn/
 }
 
-packages() {
-  if [ -e "/etc/apt/sources.list.d/werner-jaeger-ppa-werner-vpn-trusty.list" ]; then
-    echo "Repositório para instalação da VPN já existe. Pulando sua inclusão"
+packages_step_done() {
+  if false; then
+    echo 'packages_step_done...true'
+    return 0
   else
-    echo "Adicionando repositório para instalação da VPN"
-    sudo apt-add-repository ppa:werner-jaeger/ppa-werner-vpn
+    echo 'packages_step_done...false'
+    return 1
   fi
+}
 
-  if [ -e "/etc/apt/sources.list.d/pipelight-stable-precise.list" ]; then
+packages_step() {
+  # if [ -e "/etc/apt/sources.list.d/werner-jaeger-ppa-werner-vpn-trusty.list" ]; then
+    # echo "Repositório para instalação da VPN já existe. Pulando sua inclusão"
+  # else
+    # echo "Adicionando repositório para instalação da VPN"
+    # sudo apt-add-repository ppa:werner-jaeger/ppa-werner-vpn
+    # update_pkgs true
+  # fi
+
+  if [ -e "/etc/apt/sources.list.d/pipelight-stable-trusty.list" ]; then
     echo "Repositório para instalação do Netflix Desktop já existe. Pulando sua inclusão"
   else
     echo "Adicionando repositório para instalação do Netflix Desktop"
     sudo apt-add-repository ppa:pipelight/stable
+    update_pkgs true
     # Se falhar por causa das fontes, chamar o comando abaixo
     # sudo apt-get --purge --reinstall install ttf-mscorefonts-installer
   fi
@@ -126,11 +216,21 @@ packages() {
   fi
 
   echo "Atualizando e instalando todos os pacotes desejados"
-  sudo apt-get update
-  sudo apt-get --yes --force-yes install vim xbacklight powertop curl screen radiotray filezilla l2tp-ipsec-vpn pdfshuffler planner gimp nfs-kernel-server nfs-common portmap
+  install_pkgs 'vim apache2-utils xbacklight powertop curl screen radiotray filezilla pdfshuffler gimp nfs-kernel-server nfs-common l2tp-ipsec-vpn'
+  # install_pkgs 'l2tp-ipsec-vpn'
 }
 
-rvm() {
+rvm_step_done() {
+  if false; then
+    echo 'rvm_step_done...true'
+    return 0
+  else
+    echo 'rvm_step_done...false'
+    return 1
+  fi
+}
+
+rvm_step() {
   echo "Instalando o rvm"
   \curl -L https://get.rvm.io | bash -s stable
   rm -f ~/.profile
@@ -145,7 +245,17 @@ rvm() {
   ./update_bundles
 }
 
-virtualbox() {
+virtualbox_step_done() {
+  if false; then
+    echo 'virtualbox_step_done...true'
+    return 0
+  else
+    echo 'virtualbox_step_done...false'
+    return 1
+  fi
+}
+
+virtualbox_step() {
   if grep -q "download.virtualbox.org" /etc/apt/sources.list; then
     echo "Repositório do VirtualBox já existe no sources. Pulando sua inclusão"
   else
@@ -164,9 +274,19 @@ virtualbox() {
   echo "Agora configure o VirtualBox (baixar e instalar os additionals e importar as VMs base)..."
 }
 
-vagrant() {
+vagrant_step_done() {
+  if false; then
+    echo 'vagrant_step_done...true'
+    return 0
+  else
+    echo 'vagrant_step_done...false'
+    return 1
+  fi
+}
+
+vagrant_step() {
   echo "Após iniciar o VirtualBox e colocar a VM base nele, executar este para instalar o Vagrant"
-  VAGRANT_VERSION=vagrant_1.5.4_x86_64.deb
+  VAGRANT_VERSION=vagrant_1.6.5_x86_64.deb
   wget https://dl.bintray.com/mitchellh/vagrant/$VAGRANT_VERSION
   sudo dpkg -i ~/$VAGRANT_VERSION
   rm ~/$VAGRANT_VERSION
@@ -174,48 +294,53 @@ vagrant() {
   vagrant box add debian-wheezy-amd64-base debian-wheezy-amd64-base.box
 }
 
+check_flow() {
+  echo "Missing implementation but I wish to list the status of the installation flow and point to what is missing"
+  #TODO: each step must have a standard check with boolean response to say if the step is done or not
+  first_step_done
+}
+
 case "$1" in
   first)
-    first
-    ;;
-  git)
-    git
-    ;;
-  gdrive)
-    gdrive
-    ;;
-  bash)
-    bash
-    ;;
-  ssh)
-    ssh
+    first_step
     ;;
   packages)
-    packages
+    packages_step
+    ;;
+  gdrive)
+    gdrive_step
+    ;;
+  ssh)
+    ssh_step
+    ;;
+  git)
+    git_step
     ;;
   rvm)
-    rvm
+    rvm_step
     ;;
   virtualbox)
-    virtualbox
+    virtualbox_step
     ;;
   vagrant)
-    vagrant
+    vagrant_step
+    ;;
+  check_flow)
+    check_flow
     ;;
   setup)
-    bash
-    ssh
-    packages
-    git
-    rvm
+    first_step
+    packages_step
+    ssh_step
+    git_step
+    rvm_step
     ;;
   *)
-    echo "Usage: $0 {first|git|gdrive|bash|ssh|packages|rvm|virtualbox|vagrant}"
+    echo "Usage: $0 {first|packages|gdrive|ssh|git|rvm|virtualbox|vagrant}"
     echo ""
 #    echo "Details"
 #    echo "  first:     first update on packages"
 #    echo "  copy:      copy basic files such as ssh keys and virtual box debian image (used by vagrant). Must have the dirs 'googledrive/ssh' and 'Área de Trabalho/debian...'"
-#    echo "  bash:      config bash scripts and vim. Must have finished the Grive sync so the dirs 'googledrive/bash' and 'googledrive/vim' exists"
 #    echo "  packages:  install all basic packages such as virtual box, git, vim and so on"
 #    echo "  git:       configure git, merger etc"
 #    echo "  rvm:       install and set rvm to use ruby"
