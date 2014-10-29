b4b441d28f8d5aee61a0f2cfb9caf577075807ff
Daniel Silvestre <daniel.silvestre@locaweb.com.br>
2014-10-29 18:04:48 -0200

    First script copied from pen drive to startup the installation

---
+++ prov_start.sh
@@ -0,0 +1,5 @@
+cp -r /media/dani/2202-A5CD/prov_files/vbox /tmp
+cd ~
+\wget https://raw.githubusercontent.com/djlebersilvestre/bash/master/provision.sh
+chmod +x provision.sh
+./provision.sh setup
