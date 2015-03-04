b458aa972dd08637801df8066be01fcc202f460d
Daniel Silvestre <daniel.silvestre@locaweb.com.br>
2015-03-04 17:53:12 -0300

    Changing the cloud storage to Copy and other bugfixes - got it while updating my OS

--- prov_start.sh
+++ prov_start.sh
@@ -1,4 +1,3 @@
-cp -r /media/dani/2202-A5CD/prov_files/vbox /tmp
 cd ~
 \wget https://raw.githubusercontent.com/djlebersilvestre/bash/master/provision.sh
 chmod +x provision.sh
--- provision.sh
+++ provision.sh
@@ -2,17 +2,17 @@
 
 RETVAL=0
 
-is_gdrive_working() {
-  if [ ! -d "$HOME/gdrive" ] || [ ! -d "$HOME/gdrive/ssh" ] || [ ! -d "$HOME/gdrive/ssh/vpn" ]; then
+is_copy_working() {
+  if [ ! -d "$HOME/Copy" ] || [ ! -d "$HOME/Copy/ssh" ] || [ ! -d "$HOME/Copy/ssh/vpn" ]; then
     return 1
   else
     return 0
   fi
 }
 
-check_gdrive() {
-  if ! is_gdrive_working; then
-    echo "O Gdrive não está configurado ou inicializado. Abortando o processo"
+check_copy() {
+  if ! is_copy_working; then
+    echo "The Copy (cloud storage) is not configured or initialized. Aborting the process"
     exit 1
   fi
 }
@@ -31,23 +31,23 @@ install_pkgs() {
   pkgs=$1
 
   if [ -z "$pkgs" ]; then
-    echo "É necessário passar os pacotes separados por espaço como argumento"
+    echo "The packages must be passed as argument, separated by spaces to proceed with the installation"
     exit 1
   fi
 
   update_pkgs
 
-  echo "Instalando pacotes $pkgs"
+  echo "Installing packages $pkgs"
   sudo apt-get --yes --force-yes install $pkgs
 }
 
 first_step() {
   if [ "$PROV_FIRST_ROOT_PASS" = true ]; then
-    echo "A senha do root já foi configurada"
+    echo "The root password has already been set"
   else
     exit_code=1
     while [ $exit_code != 0 ]; do
-      echo "Por favor, configure a senha do root"
+      echo "Please, set up your new root password"
       sudo passwd root
       exit_code=$?
     done
@@ -57,25 +57,25 @@ first_step() {
   fi
 
   if [ "$PROV_FIRST_UPDATE" = true ]; then
-    echo "Os pacotes da distro já foram autalizados"
+    echo "The packages are up to date"
   else
-    echo "Primeira execução. Atualizando todos os pacotes da distro"
+    echo "First time running this script. Updating all packages"
     sudo apt-get update
     sudo apt-get --yes --force-yes upgrade
 
     export PROV_FIRST_UPDATE=true
     echo 'export PROV_FIRST_UPDATE=true' >> ~/.bashrc
 
-    echo "Reiniciando para aplicar todas atualizações"
+    echo "Rebooting to apply all updates"
     sudo reboot
   fi
 }
 
 git_step() {
-  echo "Atualizando e instalando o git"
+  echo "Updating and installing git"
   install_pkgs 'git meld gitk'
 
-  echo "Configurando o git"
+  echo "Configuring git"
   sudo rm -f /usr/local/bin/git-diff.sh
   echo '#!/bin/bash' >> git-diff.sh
   echo 'meld "$2" "$5" > /dev/null 2>&1' >> git-diff.sh
@@ -88,7 +88,7 @@ git_step() {
   git config --global color.ui true
   git config --global diff.external /usr/local/bin/git-diff.sh
 
-  echo "Configurando o bash e vimrc"
+  echo "Configuring bash and vimrc"
   git clone https://github.com/djlebersilvestre/vim ~/.vim
   git clone https://github.com/djlebersilvestre/bash ~/.bash
   rm -f ~/.bashrc ~/.bash_profile ~/.profile ~/.vimrc
@@ -97,104 +97,82 @@ git_step() {
   ln -s ~/.vim/vimrc ~/.vimrc
 }
 
-gdrive_step() {
-  if [ -e "/etc/apt/sources.list.d/alessandro-strada-ppa-trusty.list" ]; then
-    echo "Repositório para instalação do Gdrive já existe. Pulando sua inclusão"
-  else
-    echo "Adicionando repositório para instalação do Gdrive (google-drive-ocamlfuse)"
-    sudo apt-add-repository ppa:alessandro-strada/ppa
-  fi
-
-  install_pkgs 'google-drive-ocamlfuse'
-
-  mkdir -p ~/gdrive
-  sudo usermod -a -G fuse $USER
-  source ~/.bashrc
-  echo "Será aberto o navegador para configurar e autorizar o Google Drive a acessar dados do google. Favor proceder com as configuracoes"
-  google-drive-ocamlfuse
-  google-drive-ocamlfuse ~/gdrive
-  echo "Para montar o drive (google-drive-ocamlfuse ~/gdrive). Para desmontar (fusermount -u ~/gdrive)"
-  echo "Mais detalhes tais como automount em http://xmodulo.com/2013/10/mount-google-drive-linux.html"
-}
-
 ssh_step() {
-  check_gdrive
+  check_copy
 
-  echo "Copiando chaves ssh e configurando pasta no home"
+  echo "Copying ssh keys and configuring at home directory"
   mkdir -p ~/.ssh
-  cp -R ~/gdrive/ssh/* ~/.ssh
+  cp -R ~/Copy/ssh/* ~/.ssh
   chmod 600 ~/.ssh/vpn/*
   chmod 600 ~/.ssh/*
   chmod 700 ~/.ssh/vpn/
 }
 
 packages_step() {
-  #TODO: find alternatives to locaweb VPN
-  # if [ -e "/etc/apt/sources.list.d/werner-jaeger-ppa-werner-vpn-trusty.list" ]; then
-    # echo "Repositório para instalação da VPN já existe. Pulando sua inclusão"
-  # else
-    # echo "Adicionando repositório para instalação da VPN"
-    # sudo apt-add-repository ppa:werner-jaeger/ppa-werner-vpn
-    # update_pkgs true
-    # install_pkgs 'l2tp-ipsec-vpn'
-  # fi
-
   should_update_pkgs=false
+
   if [ -e "/etc/apt/sources.list.d/pipelight-stable-trusty.list" ]; then
-    echo "Repositório para instalação do Netflix Desktop já existe. Pulando sua inclusão"
+    echo "Netflix repository already exists. Bypassing this install"
   else
-    echo "Adicionando repositório para instalação do Netflix Desktop"
+    echo "Adding repository to install Netflix Desktop"
     sudo apt-add-repository ppa:pipelight/stable
     should_update_pkgs=true
   fi
 
   if [ -e "/etc/apt/sources.list.d/google.list" ]; then
-    echo "Repositório para instalação do Google Chrome já existe. Pulando sua inclusão"
+    echo "Google Chrome repository already exists. Bypassing this install"
   else
-    echo "Adicionando repositório para instalação do Google Chrome"
+    echo "Adding repository to install Google Chrome"
     wget -q https://dl-ssl.google.com/linux/linux_signing_key.pub -O- | sudo apt-key add -
     sudo bash -c "echo deb http://dl.google.com/linux/chrome/deb/ stable main >> /etc/apt/sources.list.d/google.list"
     should_update_pkgs=true
   fi
 
-  if [ -e "/lib/x86_64-linux-gnu/libudev.so.0" ]; then
-    echo "Lib já linkada, pulando fix do popcorn time"
+  if [ -e "/etc/apt/sources.list.d/webupd8team-java-trusty.list" ]; then
+    echo "Oracle Java 8 repository already exists. Bypassing this install"
   else
-    echo "Aplicando fix do popcorn time"
+    echo "Adding repository to install Oracle Java 8"
+    echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+    sudo add-apt-repository ppa:webupd8team/java
+    should_update_pkgs=true
+  fi
+
+  if [ -e "/lib/x86_64-linux-gnu/libudev.so.0" ]; then
+    echo "Lib already linked, bypassing PopcornTime fix"
+  else
+    echo "Applying PopcornTime fix"
     sudo ln -s /lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libudev.so.0
   fi
 
-  if should_update_pkgs; then
+  if $should_update_pkgs; then
     update_pkgs true
   fi
 
-  echo "Atualizando e instalando todos os pacotes desejados"
-  install_pkgs 'vim apache2-utils xbacklight powertop curl screen htop radiotray filezilla pdfshuffler gimp nfs-kernel-server nfs-common google-chrome-stable netflix-desktopi whois'
+  echo "Updating and installing all desired packages"
+  install_pkgs 'vim apache2-utils xbacklight curl screen htop pdfshuffler gimp google-chrome-stable netflix-desktop whois oracle-java8-installer'
+  # standing by: powertop radiotray filezilla https://code.google.com/p/gitinspector/downloads/list
 
-  # Dependencias para o Netflix Desktop
+  # Dependencies for Netflix
   sudo apt-get --purge --reinstall --yes --force-yes install ttf-mscorefonts-installer
 
-  echo "Não esqueça de:"
-  echo " - Setar em aplicativos de sessão o brilho do monitor (/usr/bin/xbacklight -set 70)"
-  echo " - Rodar a primeira vez o Netflix Desktop para configurá-lo"
-  echo " - Adicionar apps no launcher: Chrome, Netflix"
+  echo "Do not forget to:"
+  echo " - Setup into Startup Applications the brightness of the display (/usr/bin/xbacklight -set 70)"
+  echo " - Run Netflix for the first time to setup account and password"
+  echo " - Add apps into the launcher: Chrome, Netflix"
 }
 
 rvm_step() {
-  echo "Instalando o rvm"
+  echo "Installing rvm"
   gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable
   rm -f ~/.profile
   source ~/.bash_profile
   type rvm | head -n 1
-  rvm install 1.9.2
-  rvm install 1.9.3
-  rvm install 2.1.2
-  rvm install 2.1.4
-  rvm use 2.1.2 --default
+  rvm install 2.2.1
+  rvm use 2.2.1 --default
 
   #TODO: check if it is already updated
-  echo "Instalando plugins do vim"
+  echo "Installing vim plugins"
   cd ~/.vim/
   chmod +x update_bundles
   ./update_bundles
@@ -202,52 +180,45 @@ rvm_step() {
 
 virtualbox_step() {
   if grep -q "download.virtualbox.org" /etc/apt/sources.list; then
-    echo "Repositório do VirtualBox já existe no sources. Pulando sua inclusão"
+    echo "VirtualBox repository already exists. Bypassing this install"
   else
-    echo "Adicionando VirtualBox como sources no apt"
+    echo "Adding repository to install VirtualBox"
     wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
     sudo bash -c "echo deb http://download.virtualbox.org/virtualbox/debian trusty contrib >> /etc/apt/sources.list"
   fi
 
   install_pkgs 'virtualbox-4.3'
 
-  mkdir -p ~/vbox
-  if [ -d "/tmp/vbox/debian_7.0.0-amd64-base/" ]; then
-    mv /tmp/vbox/debian_7.0.0-amd64-base/ ~/vbox/
-  fi
-  if [ -e "/tmp/vbox/debian-wheezy-amd64-base.box" ]; then
-    mv /tmp/vbox/debian-wheezy-amd64-base.box ~/vbox/
-  fi
-  rmdir /tmp/vbox
-
-  echo "Baixando o extension pack"
+  echo "Downloading VirtualBox Extension Pack"
   #TODO: auto find the version
-  VBOX_EXT_PATH=4.3.18
-  VBOX_EXT_VERSION=Oracle_VM_VirtualBox_Extension_Pack-4.3.18-96516.vbox-extpack
-
+  VBOX_EXT_PATH=4.3.24
+  VBOX_EXT_BUILD=98716
+  VBOX_EXT_VERSION=Oracle_VM_VirtualBox_Extension_Pack-$VBOX_EXT_PATH-$VBOX_EXT_BUILD.vbox-extpack
+  
   cd ~
   wget http://download.virtualbox.org/virtualbox/$VBOX_EXT_PATH/$VBOX_EXT_VERSION
 
-  echo "Agora configure o VirtualBox (instalar os additionals em $HOME/$VBOX_EXT_VERSION e importar as VMs base)..."
+  echo "Now you must setup VirtualBox (install the additionals in $HOME/$VBOX_EXT_VERSION and import the base VMs)..."
   /usr/bin/virtualbox
 
   rm $VBOX_EXT_VERSION
 }
 
 vagrant_step() {
-  echo "Após iniciar o VirtualBox e colocar a VM base nele, executar este para instalar o Vagrant"
+  echo "Run this script only after VirtualBox setup. It will install Vagrant"
 
   #TODO: auto find the version
-  VAGRANT_VERSION=vagrant_1.6.5_x86_64.deb
+  VAGRANT_VERSION=vagrant_1.7.2_x86_64.deb
 
   cd ~
   wget https://dl.bintray.com/mitchellh/vagrant/$VAGRANT_VERSION
   sudo dpkg -i ~/$VAGRANT_VERSION
 
   rm ~/$VAGRANT_VERSION
-  cd ~/vbox/
+  VBOX_DIR=$HOME/Copy/vbox/
+  cd $VBOX_DIR
 
-  if [ -d "$HOME/vbox/debian_7.0.0-amd64-base/" ] && [ -e "$HOME/vbox/debian-wheezy-amd64-base.box" ]; then
+  if [ -d "$VBOX_DIR/debian_7.0.0-amd64-base/" ] && [ -e "$VBOX_DIR/debian-wheezy-amd64-base.box" ]; then
     vagrant box add debian-wheezy-amd64-base debian-wheezy-amd64-base.box
   fi
 }
@@ -262,8 +233,8 @@ case "$1" in
   packages)
     packages_step
     ;;
-  gdrive)
-    gdrive_step
+  copy)
+    copy_step
     ;;
   ssh)
     ssh_step
@@ -278,34 +249,39 @@ case "$1" in
     vagrant_step
     ;;
   setup)
+    STEPS_NUM=6
+    echo "Step 1 / $STEPS_NUM"
     first_step
+    echo "Step 2 / $STEPS_NUM"
     git_step
+    echo "Step 3 / $STEPS_NUM"
     packages_step
-    gdrive_step
+    echo "Step 4 / $STEPS_NUM"
+    copy_step
+    echo "Step 5 / $STEPS_NUM"
     ssh_step
+    echo "Step 6 / $STEPS_NUM"
     rvm_step
-    virtualbox_step
-    vagrant_step
+    #virtualbox_step
+    #vagrant_step
+    echo "Finished all steps!"
     ;;
   *)
-    echo "Usage: $0 {setup|first|git|packages|gdrive|ssh|rvm|virtualbox|vagrant}"
+    echo "Usage: $0 {setup|first|git|packages|copy|ssh|rvm|virtualbox|vagrant}"
     echo ""
     echo "Details"
-    echo "  setup:      RECOMMENDED - triggers all: first > packages > git > gdrive > ssh > rvm > virtualbox > vagrant"
+    echo "  setup:      RECOMMENDED - triggers all: first > packages > git > copy > ssh > rvm > virtualbox > vagrant"
     echo "  first:      first update on packages and setup of root password"
     echo "  git:        configure git, merger and installs the default bash and vim scripts"
     echo "  packages:   install all basic packages such as vim, screen and so on"
-    echo "  gdrive:     install and setup the google drive (requires manual steps)"
-    echo "  ssh:        configure default SSH keys (depends on gdrive)"
+    echo "  copy:       install and setup the Copy Cloud Storage"
+    echo "  ssh:        configure default SSH keys (depends on Copy)"
     echo "  rvm:        install and set rvm to use ruby, also updates the vim bundles"
     echo "  virtualbox: install VirtualBox (requires manual steps)"
     echo "  vagrant:    install vagrant"
     echo ""
     RETVAL=1
 esac
-
-#TODO: https://code.google.com/p/gitinspector/downloads/list incluir como dependência
-#TODO: http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html automatizar instalação do Java
 
 exit $RETVAL
 
